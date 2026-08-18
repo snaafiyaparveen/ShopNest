f45789cd41cabad655f66a58cbae2822d545a0ad
Naafiya <snaafiyaparveen@gmail.com>
2026-08-19 01:28:41 +0530

    Allow Render frontend host

--- ShopNest/frontend/vite.config.js
+++ ShopNest/frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
@@ -11,5 +12,9 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+
+  preview: {
+    allowedHosts: ['shopnest-frontend-mbmw.onrender.com']
   }
 })
